6a77d4c6248bff4212ca3ed7f0abb03c5009af0e
KacperSi <kacper.siemianowski@student.put.poznan.pl>
2021-12-30 17:38:34 +0100

    Rejestracja uzytkownikow

--- mycms/settings.py
+++ mycms/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'clothes.apps.ClothesConfig',
     'general.apps.generalConfig',
+    'users.apps.usersConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +129,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK='bootstrap4'
--- mycms/urls.py
+++ mycms/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from general import views as general_views
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', general_views.home, name='home'),
+    path('register/', user_views.register, name='register'),
+
 ]
