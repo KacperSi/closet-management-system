8adf8329b84c95ab51417399965cb798a3adf921
KacperSi <kacper.siemianowski@student.put.poznan.pl>
2021-12-30 14:43:59 +0100

    Dodanie general

--- clothes/apps.py
+++ clothes/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ClothesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'clothes'
+    path = 'C:\\Users\\KacperSiemianowski\\Desktop\\CMS Projekt\\mycms\\clothes'
--- mycms/urls.py
+++ mycms/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from general import views as general_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', general_views.home, name='home'),
 ]
