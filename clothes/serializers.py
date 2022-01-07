3dbbc59e5e30f9283a14881511c046d1915e5914
wolnio <kowolniak@gmail.com>
2022-01-07 13:29:26 +0100

    Chuj wie

--- clothes/serializers.py
+++ clothes/serializers.py
@@ -4,6 +4,6 @@ from .models import Garment, WEATHER
 class GarmentSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Garment
-		fields = ('user', 'weather_type', 'cathegory', 'name', 'image', 'preference_index')
+		fields = ('user', 'weather_type', 'cathegory', 'name', 'image', 'preference_index', 'max_temp', 'min_temp')
 
 	weather_type = fields.MultipleChoiceField(choices=WEATHER)
