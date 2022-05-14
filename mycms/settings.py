9cc0365584c2b87d8366925461fdf5f6675ac225
SzymonS <szymon.sliwinski@holarsys.pl>
2022-05-14 14:48:38 +0200

    bumped django version to 3.2, deleted venv from repo

--- mycms/settings.py
+++ mycms/settings.py
@@ -50,12 +50,14 @@ INSTALLED_APPS = [
     'allauth.account',
     'rest_auth.registration',
     'multiselectfield',
-    'weather.apps.weatherConfig'
+    'weather.apps.weatherConfig',
+    'corsheaders',
 ]
 
 SITE_ID = 1
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -63,6 +65,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'mycms.urls'
